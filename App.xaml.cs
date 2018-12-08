8421d575aaab1398c4442acd3d39b14a1ae1d9f0
JamesBlack <451681592@qq.com>
2018-12-08 15:25:15 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WalterlvWpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        App()
+        {
+            this.Startup += App_Startup;
+           
+        }
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            MessageBox.Show("fasdf");
+        }
+    }
+}
